fa75db119771d63d3b1093d46dbe6237b366bbe1
Michael Epley <michaelepley@gmail.com>
2016-11-07 11:46:04 -0500

    Update install-go.sh

--- docker/install-go.sh
+++ docker/install-go.sh
@@ -8,7 +8,7 @@
 echo "  --> preparing to build source"
 cd /usr/local/src || exit 1
 echo "  --> cloning source"
-hg clone git@github.com:golang/go.git | exit 1
+git clone git@github.com:golang/go.git | exit 1
 echo "  --> building"
 cd go/src || exit 1
 ./all.bash || exit 1
