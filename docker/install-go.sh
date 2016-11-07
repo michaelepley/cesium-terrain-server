0263b23abd4c46fd6b6525098cfe3f020fc2b394
Michael Epley <michaelepley@gmail.com>
2016-11-07 11:37:50 -0500

    Update install-go.sh

--- docker/install-go.sh
+++ docker/install-go.sh
@@ -5,9 +5,12 @@
 #
 # See <https://golang.org/doc/install/source>
 #
-
+echo "  --> preparing to build source"
 cd /usr/local/src || exit 1
-hg clone https://github.com/golang/go || exit 1
+echo "  --> cloning source"
+hg clone git@github.com:golang/go.git | exit 1
+echo "  --> building"
 cd go/src || exit 1
 ./all.bash || exit 1
+echo "  --> Linking executable"
 ln -s /usr/local/src/go/bin/go* /usr/local/bin/ || exit 1
