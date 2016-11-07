9c545e4e8dfe9e5e465456d405771b5113b6290b
Michael Epley <michaelepley@gmail.com>
2016-11-07 11:29:24 -0500

    Update install-go.sh

--- docker/install-go.sh
+++ docker/install-go.sh
@@ -7,7 +7,7 @@
 #
 
 cd /usr/local/src || exit 1
-hg clone -u release https://code.google.com/p/go || exit 1
+hg clone https://github.com/golang/go || exit 1
 cd go/src || exit 1
 ./all.bash || exit 1
 ln -s /usr/local/src/go/bin/go* /usr/local/bin/ || exit 1
